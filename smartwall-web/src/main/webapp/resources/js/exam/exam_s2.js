2194e92ef80da69adf38c075382fec058264cada
lexloo <lexloo@126.com>
2017-06-25 20:38:52 +0800

    in progress

---
+++ smartwall-web/src/main/webapp/resources/js/exam/exam_s2.js
@@ -0,0 +1,35 @@
+define(function(require, exports, module) {
+	var form = require("pub/form_dms");
+
+	var _grid;
+	exports.setGrid = function(grid) {
+		_grid = grid;
+	};
+
+	var _formConfig = {
+		width: 400,
+		height: 300,
+		bizCode: 'kygj_exam_zz',
+		columns: [{
+			display: "试题名称",
+			name: "caption",
+			type: "text",
+			validate: {
+				maxLength: 100,
+				required: true
+			}
+		}],
+	};
+
+	Exam = {};
+	form.initDefMethod(Exam, _formConfig);
+
+
+	Grid.showExamEditor = function() {
+		return function(rowdata, index, value) {
+			var href = CONTEXT + "exam/edit.mvc?type=S2&guid=" + rowdata["guid"];
+
+			return '<a href="' + href + '">编辑</a>';
+		};
+	};
+});
