020455511a23999276b71e599cb93936ba06afa6
lexloo <lexloo@126.com>
2017-09-17 22:02:15 +0800

    in progress

--- smartwall-forum/src/main/java/link/smartwall/cloud/forum/controller/HelloController.java
+++ smartwall-forum/src/main/java/link/smartwall/cloud/forum/controller/HelloController.java
@@ -1,20 +1,12 @@
 package link.smartwall.cloud.forum.controller;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import org.nutz.dao.Dao;
-import org.nutz.dao.Sqls;
-import org.nutz.dao.sql.Sql;
-import org.nutz.dao.sql.SqlCallback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import link.smartwall.cloud.forum.dao.ForumDao;
 import link.smartwall.cloud.forum.domain.Attach;
+import link.smartwall.cloud.forum.service.ForumService;
 
 /**
  * Hello world!
@@ -24,28 +16,11 @@ import link.smartwall.cloud.forum.domain.Attach;
 @ComponentScan
 public class HelloController {
 	@Autowired
-	private ForumDao dao;
+	private ForumService service;
 
 	@RequestMapping("/hello")
-	public String index() {
-//		Sql sql = Sqls.create("select guid, caption from kygj_exam_e1");
-//		sql.setCallback(new SqlCallback() {
-//
-//			@Override
-//			public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
-//				while (rs.next()) {
-//					System.out.println(rs.getString("guid"));
-//					System.out.println(rs.getString("caption"));
-//				}
-//
-//				return null;
-//			}
-//		});
-//		this.dao.getDao().execute(sql);
-		Attach att = new Attach();
-		att.setGuid("CYDFSF");
-		
-		dao.getDao().fastInsert(att);
+	public String index(Attach attach) {
+		service.insert(attach);
 
 		return "Hello World";
 	}
