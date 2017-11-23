1a502743a6b844243f22a0d2919b2f04e9e1c212
lexloo <lexloo@126.com>
2017-11-23 23:13:19 +0800

    ....

--- smartwall-question_bank/src/main/resources/static/src/views/Exam.vue
+++ smartwall-question_bank/src/main/resources/static/src/views/Exam.vue
@@ -1,31 +1,67 @@
-<style  lang="less">
+<style  scoped lang="less">
+    .exam {
+        font-size: 12px;
+    }
+
+    .selected {
+        color: #fff !important;
+        background-color: #04be02;
+    }
+
+    #question-type {
+        border: 1px solid #ddd;
+    }
+
+    #type {
+        height: 32px;
+        line-height: 32px;
+        margin-left: 20px;
+    }
+
+    #stem {
+        height: 32px;
+        line-height: 32px;
+        margin-left: 20px;
+    }
+
+    #options {
+        margin-left: 20px;
+    }
+    
+    .option {
+        margin: 4px 8px;
+        color: #04be02;
+        border: 1px solid #04be02;
+        width: 24px;
+        height: 24px;
+        display: inline-block;
+        line-height: 24px;
+        text-align: center;
+        border-radius: 12px;
+    }
 </style>
 
 <template>
-    <div class="exam" style="padding: 20px">
-        <div>
-            <router-link :to="{path:'/Chapter'}" class="return" title="">返回</router-link>
+    <div class="exam">
+        <x-header></x-header>
+        <div id="question-type">
+            <span id="type">{{current.type}}</span>
         </div>
-        <div class="aui-header">
-            <h4></h4>
-        </div>
-        <div class="question-type">
-            <span>{{current.type}}</span>
-        </div>
-        <div>{{current.text}}</div>
-        <div>
-            <div @click="select('A')"><span :class="{'selected': current.select == 'A'}">A</span>{{current.opt_A}}</div>
-            <div @click="select('B')"><span :class="{'selected': current.select == 'B'}">B</span>{{current.opt_B}}</div>
-            <div @click="select('C')"><span :class="{'selected': current.select == 'C'}">C</span>{{current.opt_C}}</div>
-            <div @click="select('D')"><span :class="{'selected': current.select == 'D'}">D</span>{{current.opt_D}}</div>
-            <div @click="select('E')"><span :class="{'selected': current.select == 'E'}">E</span>{{current.opt_E}}</div>
+        <div id="stem">{{current.text}}</div>
+        <div id="options">
+            <div @click="select('A')"><span class="option" :class="{'selected': current.select == 'A'}">A</span>{{current.opt_A}}</div>
+            <div @click="select('B')"><span class="option" :class="{'selected': current.select == 'B'}">B</span>{{current.opt_B}}</div>
+            <div @click="select('C')"><span class="option" :class="{'selected': current.select == 'C'}">C</span>{{current.opt_C}}</div>
+            <div @click="select('D')"><span class="option" :class="{'selected': current.select == 'D'}">D</span>{{current.opt_D}}</div>
+            <div @click="select('E')"><span class="option" :class="{'selected': current.select == 'E'}">E</span>{{current.opt_E}}</div>
         </div>
     </div>        
 </template>
 
 <script>
     import examData from '../data/exam_data';
-
+    import { Divider, XHeader } from 'vux';
+    
     export default {
         name: 'Exam',
         data() {
@@ -34,13 +70,23 @@
             };
         },
         props: {},
-        computed: {},
+        computed: {
+            count() {
+                return this.questions.length;
+            }
+        },
+        components: {
+            Divider,
+            XHeader
+        },
         methods: {
             select: function(answer) {
                 this.current.select = answer;
+
+                this.next();
             },
             next: function() {
-                if (this.no == this.count() - 1) {
+                if (this.no == this.count - 1) {
                     alert('已经最后');
                 } else {
                     this.no++;
