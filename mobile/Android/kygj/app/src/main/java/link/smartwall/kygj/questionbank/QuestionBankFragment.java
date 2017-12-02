4d11618450c69806ea1071a03c1c41a414ee0a99
lexloo <lexloo@126.com>
2017-12-02 11:21:37 +0800

    ...

--- mobile/Android/kygj/app/src/main/java/link/smartwall/kygj/questionbank/QuestionBankFragment.java
+++ mobile/Android/kygj/app/src/main/java/link/smartwall/kygj/questionbank/QuestionBankFragment.java
@@ -9,16 +9,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import link.smartwall.controls.view.TitleView;
 import link.smartwall.kygj.R;
 
 public class QuestionBankFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private List<Chapter> dataBeanList;
     private RecyclerAdapter mAdapter;
+    private TitleView titleView;
 
     public QuestionBankFragment() {
     }
@@ -43,8 +46,26 @@ public class QuestionBankFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         mRecyclerView = (RecyclerView) view.findViewById(R.id.recycle_view);
+        titleView = (TitleView) view.findViewById(R.id.question_bank_titleview);
 
-        initData();
+        initEvents();
+//        initData();
+    }
+
+    private void initEvents() {
+        this.titleView.getLeftBackTextTv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "点击返回，可在此处调用left()", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        this.titleView.getRightTextTv().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "点击返回，可在此处调用right()", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void initData() {
