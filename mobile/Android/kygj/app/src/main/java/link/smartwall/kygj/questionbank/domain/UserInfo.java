a0269140195720a4624dcc79d34c0e47804c9ba6
lexloo <lexloo@126.com>
2017-12-09 15:27:39 +0800

    ...

---
+++ mobile/Android/kygj/app/src/main/java/link/smartwall/kygj/questionbank/domain/UserInfo.java
@@ -0,0 +1,97 @@
+package link.smartwall.kygj.questionbank.domain;
+
+import org.xutils.db.annotation.Column;
+
+import java.util.Date;
+
+/**
+ * 用户信息
+ */
+public abstract class UserInfo {
+    @Column(name = "guid", isId = true)
+    private String guid;
+
+    /**
+     * 手机号
+     */
+    @Column(name = "mobile")
+    private String mobile;
+
+    /**
+     * 名称
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * 考试种类
+     */
+    @Column(name = "exam_kind")
+    private String examKind;
+
+    /**
+     * 考试类别
+     */
+    @Column(name = "exam_category")
+    private String examCategory;
+
+    /**
+     * 更新时间
+     */
+    @Column(name = "modify_time")
+    private Date modifyTime;
+
+    /**
+     * 个人图像
+     */
+    @Column(name = "avator")
+    private String avator;
+
+    public String getGuid() {
+        return guid;
+    }
+
+    public void setGuid(String guid) {
+        this.guid = guid;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExamKind() {
+        return examKind;
+    }
+
+    public void setExamKind(String examKind) {
+        this.examKind = examKind;
+    }
+
+    public String getExamCategory() {
+        return examCategory;
+    }
+
+    public void setExamCategory(String examCategory) {
+        this.examCategory = examCategory;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
